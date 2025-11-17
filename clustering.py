537536c76ad56acd16f758adfb58a4880e925a87
Muhammad Azmi Falah <azmi.falah420@gmail.com>
2025-11-17 20:52:33 +0700

    Create clustering.py
    Inisialisasi file projek

---
+++ clustering.py
@@ -0,0 +1,10 @@
+# %% 
+import pandas as pd
+
+# %%
+df = pd.read_csv('https://raw.githubusercontent.com/azmihalaf/analisis_sentimen_reku/refs/heads/main/ulasan_coc%20(1).csv')
+df.head()
+
+# %%
+df.info()
+# %%
